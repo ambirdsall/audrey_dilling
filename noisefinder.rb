e5df704fbe2e74e111242779469e4c01d5b16f88
ambirdsall <ambirdsall@gmail.com>
2015-05-13 14:52:10 -0700

    Check ENV for auth data first to avoid uninitialized constant error.

--- noisefinder.rb
+++ noisefinder.rb
@@ -4,8 +4,8 @@ require 'soundcloud'
 AUTH_DATA = YAML.load_file("secrets.yml") if File.exist?("secrets.yml")
 
 class Noisefinder
-  @@user_id = AUTH_DATA["soundcloud"]["user_id"] || ENV["AUDREY_SC_USER_ID"]
-  @@app_id = AUTH_DATA["soundcloud"]["app_id"] || ENV["SC_APP_ID"]
+  @@user_id = ENV["AUDREY_SC_USER_ID"] || AUTH_DATA["soundcloud"]["user_id"]
+  @@app_id = ENV["SC_APP_ID"] || AUTH_DATA["soundcloud"]["app_id"]
   def self.last_five_tracks
     @_last_five ||= soundcloud_client.get("/users/#{@@user_id}/tracks").last(5)
   end
