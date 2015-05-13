6a3a0796d09520d7e966fe4a3f57d794ab5306e3
ambirdsall <ambirdsall@gmail.com>
2015-05-12 17:48:25 -0700

    Find ids of Audrey's last 5 soundcloud tracks for Sounds page

--- index.rb
+++ index.rb
@@ -1,9 +1,12 @@
 require 'sinatra'
+require_relative 'noisefinder'
 
 get '/' do
   haml :index
 end
 
 get '/sounds' do
-  haml :sounds
+  track_ids = Noisefinder.last_five_tracks.map(&:id)
+
+  haml :sounds, :locals => {:track_ids => track_ids}
 end
